b93d2aedf049f83a5087fae5f15eb8acdf6b221c
Izumi <renyong.wang@thoughtworks.com>
2023-03-01 17:51:09 +0800

    add acceptance test

--- src/test/java/thoughtworks/lephora/server/lephoraserver/rest/HealthCheckApiTest.java
+++ src/test/java/thoughtworks/lephora/server/lephoraserver/rest/HealthCheckApiTest.java
@@ -2,7 +2,6 @@ package thoughtworks.lephora.server.lephoraserver.rest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
